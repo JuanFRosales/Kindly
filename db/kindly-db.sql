8ad217671844a3b9f33381f171996534c16987d4
Khai Chan <32737124+flamingraptor70@users.noreply.github.com>
2024-03-21 13:38:06 +0200

    database created

---
+++ db/kindly-db.sql
@@ -0,0 +1,82 @@
+-- Drop the database if it exists and then create it
+DROP DATABASE IF EXISTS KindlyApp;
+CREATE DATABASE KindlyApp;
+USE KindlyApp;
+
+-- Create the tables
+
+CREATE TABLE UserLevels (
+    level_id INT AUTO_INCREMENT PRIMARY KEY,
+    level_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    user_level_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_level_id) REFERENCES UserLevels(level_id)
+);
+
+CREATE TABLE MediaItems (
+    media_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    filename VARCHAR(255) NOT NULL,
+    filesize INT NOT NULL,
+    media_type VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Comments (
+    comment_id INT AUTO_INCREMENT PRIMARY KEY,
+    media_id INT NOT NULL,
+    user_id INT NOT NULL,
+    comment_text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (media_id) REFERENCES MediaItems(media_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Approvals (
+    approval_id INT AUTO_INCREMENT PRIMARY KEY,
+    media_id INT NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (media_id) REFERENCES MediaItems(media_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+
+-- Insert the sample data
+
+INSERT INTO UserLevels (level_name) VALUES ('Admin'), ('User'), ('Company'), ('Guest'),;
+
+INSERT INTO Users (username, password, email, user_level_id) VALUES
+('McDonalds', 'password123', 'mcdonalds@donalds.com', 4)
+('JohnDoe', 'to-be-hashed-pw1', 'johndoe@example.com', 2),
+('JaneSmith', 'to-be-hashed-pw2', 'janesmith@example.com', 2),
+('Anon5468', 'to-be-hashed-pw3', 'anon5468@example.com', 2),
+('AdminUser', 'to-be-hashed-pw4', 'adminuser@example.com', 1);
+
+INSERT INTO MediaItems (user_id, filename, filesize, media_type, title, description) VALUES
+(1, 'sunset.jpg', 1024, 'image/jpeg', 'Sunset', 'A beautiful sunset'),
+(2, 'sample.mp4', 20480, 'video/mp4', 'Sample Video', 'A sample video file'),
+(2, 'ffd8.jpg', 2048, 'image/jpeg', 'Favorite food', null),
+(1, '2f9b.jpg', 1024, 'image/jpeg', 'Aksux and Jane', 'friends');
+
+INSERT INTO Comments (media_id, user_id, comment_text) VALUES
+(1, 2, 'This is a wonderful photo!'),
+(2, 1, 'Really nice video, thanks for sharing!');
+
+INSERT INTO Approvals (media_id, user_id) VALUES
+(1, 2),
+(2, 1),
+(2, 2),
+(3, 1),
+(2, 3),
+(3, 3);
