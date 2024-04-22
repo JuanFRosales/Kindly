ee0e2ebe9c61dc29447e2d55c90a07f676cab6f2
DuckBat <32737124+duckbat@users.noreply.github.com>
2024-04-22 10:34:35 +0300

    added react native paper

--- client/App.tsx
+++ client/App.tsx
@@ -1,68 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the UI Kitten TypeScript template
- * https://github.com/akveo/react-native-ui-kitten
- *
- * Documentation: https://akveo.github.io/react-native-ui-kitten/docs
- *
- * @format
- */
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {PaperProvider} from 'react-native-paper';
 
-import React from 'react';
-import {ImageProps, StyleSheet} from 'react-native';
-import {
-  ApplicationProvider,
-  Button,
-  Icon,
-  IconRegistry,
-  Layout,
-  Text,
-} from '@ui-kitten/components';
-import {EvaIconsPack} from '@ui-kitten/eva-icons';
-import * as eva from '@eva-design/eva';
-
-/**
- * Use any valid `name` property from eva icons (e.g `github`, or `heart-outline`)
- * https://akveo.github.io/eva-icons
- */
-const HeartIcon = (
-  props?: Partial<ImageProps>,
-): React.ReactElement<ImageProps> => <Icon {...props} name="heart" />;
-
-export default (): React.ReactElement => (
-  <>
-    <IconRegistry icons={EvaIconsPack} />
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <Layout style={styles.container}>
-        <Text style={styles.text} category="h1">
-          Welcome to UI Kitten 😻
-        </Text>
-        <Text style={styles.text} category="s1">
-          Start with editing App.js to configure your App
-        </Text>
-        <Text style={styles.text} appearance="hint">
-          For example, try changing theme to Dark by using eva.dark
-        </Text>
-        <Button style={styles.likeButton} accessoryLeft={HeartIcon}>
-          LIKE
-        </Button>
-      </Layout>
-    </ApplicationProvider>
-  </>
-);
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <PaperProvider>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+      </PaperProvider>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-  },
-  text: {
-    textAlign: 'center',
-  },
-  likeButton: {
-    marginVertical: 16,
+    justifyContent: 'center',
   },
 });
--- client/babel.config.js
+++ client/babel.config.js
@@ -1,3 +1,11 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
+  };
 };
