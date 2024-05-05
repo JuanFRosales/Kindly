bb98a7591a08d4ed69f05f11876382a5450dd3ca
DuckBat <32737124+duckbat@users.noreply.github.com>
2024-05-05 22:09:31 +0300

    fixed api-server

--- server/api-server/src/index.ts
+++ server/api-server/src/index.ts
@@ -1,8 +1,10 @@
 //Express server
-
 import app from './app';
 
-const port = Number(process.env.PORT) || 3000;
-app.listen(port, '0.0.0.0', () => {
-  console.log("server is listening");
-})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
+});
