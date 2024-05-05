ddd31a926957608114b41f5d786277598605d5f7
DuckBat <32737124+duckbat@users.noreply.github.com>
2024-05-05 20:01:44 +0300

    port fix

--- server/api-server/src/index.ts
+++ server/api-server/src/index.ts
@@ -2,7 +2,7 @@
 
 import app from './app';
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port http://localhost:${port}`);
-});
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log("server is listening");
+})
